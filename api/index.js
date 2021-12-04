613f419df86a75aef50d9568f49d25dd51a2b053
Luis Aguilar <60889615+LuisEA97@users.noreply.github.com>
2021-12-03 21:34:55 -0600

    Corregido asyn await en index

--- api/index.js
+++ api/index.js
@@ -21,8 +21,9 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+conn.sync({ force: false }).then(
+async () => {
+ await server.listen(process.env.PORT, () => {
     console.log('%s listening at ' + process.env.PORT); // eslint-disable-line no-console
   });
 });

fa46aaab7369b7c92aeca331a1553eebec6228a9
Luis Aguilar <luisenrique.aguilares@gmail.com>
2021-12-05 17:37:51 -0600

    trying to solve origin cors

--- api/src/app.js
+++ api/src/app.js
@@ -2,21 +2,24 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors = require('cors')
+
 require('./db.js');
 
 const server = express();
 
 server.name = 'API';
 
-const whitelist = [
-  'https://react-node-pokedex.vercel.app/', 
-  'https://react-node-pokedex-git-main-luisea97.vercel.app/', 
-  'https://react-node-pokedex-git-dev-luisea97.vercel.app/', 
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'http://localhost:3002',
-]
+const cors = {
+  whitelist: [
+    'https://react-node-pokedex.vercel.app/', 
+    'https://react-node-pokedex-git-main-luisea97.vercel.app/', 
+    'https://react-node-pokedex-git-dev-luisea97.vercel.app/', 
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+  ],
+  default: 'https://react-node-pokedex.vercel.app/',
+}
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -26,12 +29,21 @@ const corsOptions = {
     }
   }
 }
-
-server.use(cors(corsOptions))
+const findURL = (url) => {
+  if(whitelist.indexOf(url) !== -1) return url
+}
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use((req, res, next) => {
+  var origin = cors.whitelist.indexOf(req.header('origin').toLowerCase()) > -1 ? req.headers.origin : cors.default;
+  res.header('Access-Control-Allow-Origin', origin); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 server.use('/', routes);
 

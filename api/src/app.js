2c51f95bb8bfabc99046e07df918c696ec84ed8f
Luis Aguilar <luisenrique.aguilares@gmail.com>
2021-12-05 17:09:22 -0600

    using cors module with cors options

--- api/src/app.js
+++ api/src/app.js
@@ -9,14 +9,25 @@ const server = express();
 
 server.name = 'API';
 
-server.use(cors())
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+const whitelist = [
+  'https://react-node-pokedex.vercel.app/', 
+  'https://react-node-pokedex-git-main-luisea97.vercel.app/', 
+  'https://react-node-pokedex-git-dev-luisea97.vercel.app/', 
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002',
+]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+server.use(cors(corsOptions))
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());

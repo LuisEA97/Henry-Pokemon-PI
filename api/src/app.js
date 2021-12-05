6be44b701ac7d1d97506b709f7d77b509a2b5f0f
Luis Aguilar <luisenrique.aguilares@gmail.com>
2021-12-05 15:42:36 -0600

    nodemon as dev dependency

--- api/src/app.js
+++ api/src/app.js
@@ -10,10 +10,6 @@ const server = express();
 server.name = 'API';
 
 server.use(cors())
-server.use(express.urlencoded({ extended: true, limit: '50mb' }));
-server.use(express.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
@@ -21,6 +17,10 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
 
 server.use('/', routes);
 

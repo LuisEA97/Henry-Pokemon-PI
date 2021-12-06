9740bab185b74c230d0c1b1f1552c3b7a909abd5
Luis Aguilar <luisenrique.aguilares@gmail.com>
2021-12-05 18:18:09 -0600

    trying (again) to fix cors

--- api/src/app.js
+++ api/src/app.js
@@ -9,18 +9,19 @@ const server = express();
 
 server.name = 'API';
 
+server.use(morgan('dev'));
+server.use(express.json());
+
+
+//Cors
 server.use(cors())
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://react-node-pokedex-git-dev-luisea97.vercel.app/'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
 });
-server.use(express.urlencoded({ extended: true, limit: '50mb' }));
-server.use(express.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use(morgan('dev'));
 
 server.use('/', routes);
 
